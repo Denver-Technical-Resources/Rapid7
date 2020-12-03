f50f71591f26029f2d0a4773872d202762e43a94
nvrenuf <lee@leecuevas.com>
2020-12-03 14:00:27 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install screen -y
+wget https://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin
+sudo chmod +x Rapid7Setup-Linux64.bin
+Sudo ./Rapid7Setup-Linux.bin
